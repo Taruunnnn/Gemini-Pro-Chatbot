fa75a2e83515eafba3212a61c810007e5ecfd849
Tarun Agrawal <tarunagrawal10309@gmail.com>
2024-07-29 00:46:52 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# **Gemini Pro ChatBot**
+
+This Flask-based chat application is being developed with utilizing HTML, CSS, and JS. This project encompasses Gemini, an AI-powered conversational chatbot.
---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+class Config:
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'tarunagrawal10309@gmail.com'
+    MAIL_PASSWORD = 'kiek uuoa sxvw phzx'
